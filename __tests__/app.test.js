a033a29937d62609ad27e066f340a58fb5b2d5fb
Nesh00 <c.nenad00@gmail.com>
2022-01-18 11:10:53 +0000

    refactor tests for /api/topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,60 @@
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
 const seed = require('../db/seeds/seed.js');
+const app = require('../app');
+const request = require('supertest');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe('Name of the group', () => {
-  test('should ', () => {});
+describe.only('/api/topics', () => {
+  describe('GET', () => {
+    test('GOOD REQUEST - if pathname is correct responds with an array of topic objects', () => {
+      return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then((res) => {
+          const { topics } = res.body;
+          expect(topics).toHaveLength(3);
+
+          topics.forEach((topic) => {
+            expect(topic).toMatchObject({
+              description: expect.any(String),
+              slug: expect.any(String),
+            });
+          });
+        });
+    });
+    test('BAD REQUEST - if pathname is wrong responds with a status 404 and error message "Not Found"', () => {
+      return request(app)
+        .get('/api/topic')
+        .expect(404)
+        .then((res) => {
+          const { message } = res.body;
+          expect(message).toBe('Invalid URL');
+        });
+    });
+  });
 });
+
+// describe('/api/articles/:article_id', () => {
+//   describe('GET', () => {
+//     test('responds with the selected article object', () => {
+//       return request(app)
+//         .get('/api/articles/1')
+//         .expect(200)
+//         .then((res) => {
+//           const { selectedArticle } = res.body;
+//           expect(selectedArticle).toMatchObject({
+//             article_id: expect.any(Number),
+//             title: expect.any(String),
+//             topic: expect.any(String),
+//             author: expect.any(String),
+//             body: expect.any(String),
+//             votes: expect.any(Number),
+//             created_at: expect.any(String),
+//           });
+//         });
+//     });
+//   });
+// });
