64dec7ec5cf68fc224888b6219f5bff3a9194e94
Nesh00 <c.nenad00@gmail.com>
2022-01-18 11:14:15 +0000

    create controller and model for topics

---
+++ controllers/topics.controller.js
@@ -0,0 +1,9 @@
+const { fetchTopics } = require('../models/topics.model');
+
+exports.getTopics = (req, res, next) => {
+  return fetchTopics()
+    .then((topics) => res.status(200).send({ topics }))
+    .catch((err) => {
+      if (err) next(err);
+    });
+};
