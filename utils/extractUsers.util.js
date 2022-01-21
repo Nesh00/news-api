887bb69dc1e6bd87b986ac66c956eeff7acde0c9
Nesh00 <c.nenad00@gmail.com>
2022-01-21 11:10:15 +0000

    fix typo on extractUsers.util

--- utils/extractUsers.util.js
+++ utils/extractUsers.util.js
@@ -1,7 +1,7 @@
 const db = require('../db/connection');
 
 exports.extractUsers = async () => {
-  const { rows } = await db.query(`SELECT username FROM USERS`);
+  const { rows } = await db.query(`SELECT username FROM users`);
 
   return rows.map(({ username }) => username);
 };
