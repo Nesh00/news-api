485fca5447792e6431dbcabf5cbf36ba1c8166c6
Nesh00 <c.nenad00@gmail.com>
2022-04-25 20:08:09 +0100

    add route for addUser

--- routes/users.router.js
+++ routes/users.router.js
@@ -2,11 +2,12 @@ const express = require('express');
 const {
   getUsers,
   getUserByUsername,
+  addUser,
 } = require('../controllers/users.controller');
 const { sanitazeParams } = require('../utils/sanitazeParams.util');
 const usersRouter = express.Router();
 
-usersRouter.route('/').get(getUsers);
+usersRouter.route('/').get(getUsers).post(addUser);
 
 usersRouter.route('/:username').all(sanitazeParams).get(getUserByUsername);
 
